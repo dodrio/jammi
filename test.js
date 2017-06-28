4370dcd58b3e187fa991c6ef541147d307899db5
m31271n <m31271n@users.noreply.github.com>
2017-06-28 23:30:42 +0800

    fix: fix wrong logic for lib/pool

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import test from 'ava'
 import jammi from '.'
 
+const OFFSET = 0.01
+
 test('Pool', t => {
   const prizes = {
     '101': 500,
@@ -11,17 +13,59 @@ test('Pool', t => {
   }
 
   const probability = 0.5
-  const total = 10000
-  let winCount = 0
+  const total = 100000
+  let shoot = 0
+  let shootIds = new Set()
 
   for (let i = 0; i < total; i++) {
-    const p = new jammi.Pool(prizes, probability, () => {
-      winCount += 1
+    jammi.pool(prizes, probability, (id) => {
+      shootIds.add(id)
+      shoot += 1
     })
-    p.draw()
   }
 
-  const realProbability = winCount / total
-  // 0.7 is a magic number, right? ;)
-  t.true(realProbability < probability && realProbability > probability * 0.7)
+  // probability is approximated with value specified by `probability`
+  const realProbability = shoot / total
+  t.true(realProbability < probability + OFFSET && realProbability > probability - OFFSET)
+
+  // all prizes can be shooted
+  t.true(shootIds.has('101'))
+  t.true(shootIds.has('102'))
+  t.true(shootIds.has('103'))
+  t.true(shootIds.has('104'))
+  t.true(shootIds.has('105'))
 })
+
+// test('Period', t => {
+//   const prizes = {
+//     '101': {
+//       sum: 500,
+//       balance: 475
+//     },
+//     '102': {
+//       sum: 100,
+//       balance: 85
+//     },
+//     '103': {
+//       sum: 50,
+//       balance: 45
+//     },
+//     '104': {
+//       sum: 10,
+//       balance: 8
+//     },
+//     '105': {
+//       sum: 1,
+//       balance: 1
+//     }
+//   }
+
+//   const startTime = Date.now()
+//   const endTime = Date.now() + 60 * 1000
+
+//   // for (let i = 0; i < 1000; i++) {
+//   //   jammi.period(prizes, startTime, endTime, 1)
+//   // }
+
+//   t.true(true)
+// })
